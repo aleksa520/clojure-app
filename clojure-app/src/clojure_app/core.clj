454339e09ae07d9e0690148e481c6c2170673928
aleksa520 <ap20160012@gmail.com>
2021-01-11 19:07:57 +0100

    added port config

--- clojure-app/src/clojure_app/core.clj
+++ clojure-app/src/clojure_app/core.clj
@@ -1,19 +1,16 @@
 (ns clojure-app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer :all]
-            [compojure.route :as route])
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [config.core :refer [env]])
   (:gen-class))
 
 (defroutes app
   (GET "/" [] "<h1>Hello World!</h1>")
   (GET "/users" [] "<h1>There are no users!</h1>"))
 
-; (defn app-handler [request]
-;   {:status 200
-;    :headers {"Content-Type" "text/html"}
-;    :body "<html><body><h1>Hello!</h1></body></html>"})
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (run-jetty app {:port 80}))
+  (run-jetty (wrap-defaults app site-defaults) {:port (:port env)}))
