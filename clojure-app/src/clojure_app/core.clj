601678eb0551a49bfd23655e52cca845241701bf
aleksa520 <ap20160012@gmail.com>
2021-01-13 11:09:17 +0100

    added rum and created main and users page

--- clojure-app/src/clojure_app/core.clj
+++ clojure-app/src/clojure_app/core.clj
@@ -1,14 +1,25 @@
 (ns clojure-app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer :all]
+            [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [config.core :refer [env]])
+            [config.core :refer [env]]
+            [rum.core :refer [defc render-static-markup]])
   (:gen-class))
 
+(defc main-page []
+  [:div {:id "main-div"
+        :class "main-page-div"}
+    [:h1 "This is main page"]])
+
+(defc users-page[]
+  [:div {:id "main-div"
+          :class "main-page-div"}
+    [:h1 "This is users page"]])
+
 (defroutes app
-  (GET "/" [] "<h1>Hello World!</h1>")
-  (GET "/users" [] "<h1>There are no users!</h1>"))
+  (GET "/" [] (render-static-markup (main-page)))
+  (GET "/users" [] (render-static-markup (users-page))))
 
 (defn -main
   "I don't do a whole lot ... yet."
