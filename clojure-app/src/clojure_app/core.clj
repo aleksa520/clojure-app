b152aea578d3ce4bfd6855cbd8cffcf5ee97788a
aleksa520 <ap20160012@gmail.com>
2021-01-13 17:37:55 +0100

    added links for home and users

--- clojure-app/src/clojure_app/core.clj
+++ clojure-app/src/clojure_app/core.clj
@@ -7,21 +7,25 @@
             [rum.core :refer [defc render-static-markup]])
   (:gen-class))
 
-(defc main-page []
+(defc template [headline component]
   [:div {:id "main-div"
         :class "main-page-div"}
-    [:h1 "This is main page"]])
+    [:h1 headline]
+    [:ul {:class "nav"}
+      [:li [:a {:href "/"} "Home"]]
+      [:li [:a {:href "users"} "Users"]]]
+    (component)])
+
+(defc main-page []
+  [:p "This is the main page!"])
 
 (defc users-page[]
-  [:div {:id "main-div"
-          :class "main-page-div"}
-    [:h1 "This is users page"]])
+  [:p "This is the users page!"])
 
 (defroutes app
-  (GET "/" [] (render-static-markup (main-page)))
-  (GET "/users" [] (render-static-markup (users-page))))
+  (GET "/" [] (render-static-markup (template "Main Page" main-page)))
+  (GET "/users" [] (render-static-markup (template "Users" users-page))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (run-jetty (wrap-defaults app site-defaults) {:port (:port env)}))
